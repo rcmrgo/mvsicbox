df8e81e374c2f6b7da7d66427f0ea8ff9297065b
Jorge Camargo <jcmrgo@gmail.com>
2015-08-07 19:40:59 -0500

    avance machine del player

--- client/lib/main.js
+++ client/lib/main.js
@@ -12,7 +12,7 @@
 
     // Set the callout.
     multi.on('active', function(index){
-      $('.post-content').removeClass('callout').eq(index).addClass('callout');
+      $('.post').removeClass('callout').eq(index).addClass('callout');
     });
 
     // Go to a track by clicking on it.
